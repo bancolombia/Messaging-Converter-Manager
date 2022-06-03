a256150d72bd33b0cfbe8c3e1a22a6c2b7abbf99
Santiago Garcia Gil <sagarci@bancolombia.com.co>
2022-06-03 14:45:48 -0500

    fix suggestions by jcgalvis

--- async/freemarker-converter-async/src/main/java/co/com/bancolombia/FreeMarkerConverseAsync.java
+++ async/freemarker-converter-async/src/main/java/co/com/bancolombia/FreeMarkerConverseAsync.java
@@ -2,7 +2,6 @@ package co.com.bancolombia;
 
 import co.com.bancolombia.api.ConverseDataGatewayAsync;
 import lombok.AllArgsConstructor;
-import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -12,13 +11,11 @@ public class FreeMarkerConverseAsync implements ConverseDataGatewayAsync {
 
     private final FreeMarkerConverseSync freeMarkerConverseSync;
 
-    @SneakyThrows
     @Override
     public <T> Mono<T> xmlToObject(String xml, String templateCode, Class<T> target) {
         return Mono.fromCallable(() -> freeMarkerConverseSync.xmlToObject(xml, templateCode, target));
     }
 
-    @SneakyThrows
     @Override
     public Mono<String> jsonToXml(String json, String templateCode, Object... context) {
         return Mono.fromCallable(() -> freeMarkerConverseSync.jsonToXml(json, templateCode, context));
--- commons-converter/src/test/java/co/com/bancolombia/utils/JsonUtilsTest.java
+++ commons-converter/src/test/java/co/com/bancolombia/utils/JsonUtilsTest.java
@@ -1,11 +1,14 @@
 package co.com.bancolombia.utils;
 
 import co.com.bancolombia.commons.utils.JsonUtils;
+import co.com.bancolombia.exceptions.ConverseException;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -20,7 +23,7 @@ class JsonUtilsTest {
 
     @Test
     void stringToTypeFail() {
-        assertThrows(Exception.class, () -> JsonUtils.stringToType("Hola", Integer.class));
+        assertThrows(ConverseException.class, () -> JsonUtils.stringToType("Hola", Integer.class));
     }
 
     @AllArgsConstructor

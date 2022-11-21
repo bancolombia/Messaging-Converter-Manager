0e8c0fe4e3a6f103b35c3301cc56ad5da7b8e30e
Santiago Garcia Gil <sagarci@bancolombia.com.co>
2022-11-21 18:13:55 -0500

    remove unused imports

--- converter-manager-api/src/main/java/co/com/bancolombia/api/TemplateValidations.java
+++ converter-manager-api/src/main/java/co/com/bancolombia/api/TemplateValidations.java
@@ -1,11 +1,9 @@
 package co.com.bancolombia.api;
-
-
-import java.io.StringWriter;
 import java.util.Map;
 
 public interface TemplateValidations {
     boolean isOkResponseXmlToObject(Map<?, ?> object);
+
     boolean isOkResponseJsonToXml(String json);
 
 }
